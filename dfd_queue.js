3e4bc46ce25d71bd32d53ffef4a7cd38cb23760d
mikeycgto <mikeycgto@gmail.com>
2011-10-29 18:01:46 -0400

    add length and promise get methods; add flag for when a promise is being created so we can create so we have nested queueing.

--- dfd_queue.js
+++ dfd_queue.js
@@ -11,12 +11,13 @@
 	}
 
 	$.DeferredQueue = function(){
+		var creating = false;
 		var queue = [];
 		var promise = null;
 
 		function create(funcs){
 			var promises = build_promises(funcs);
-			return $.when.apply($, promises).then(dequeue);;
+			return $.when.apply($, promises).then(dequeue);
 		}
 
 		function dequeue(){
@@ -30,13 +31,27 @@
 			queue:function(){
 				var args = Array.prototype.slice.call(arguments);
 				
-				if (promise){
+				if (promise && promise.isResolved())
+					promise = null;
+					
+				if (promise || creating)
 					queue.push(args);
-				} else {
+				
+				if (!promise && !creating){			
+					creating = true;
 					promise = create(args);
+					creating = false;
 				}
-
+				
 				return this;
+			},
+			
+			length:function(){
+				return queue.length;
+			},
+			
+			promise:function(){
+				return promise;
 			}
 		};
 	};	
