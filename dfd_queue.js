01246b28676de0d7ff1fe0b06dfe144b3d5bf1c1
mikeycgto <mikeycgto@gmail.com>
2011-10-29 01:17:09 -0400

    some code

---
+++ dfd_queue.js
@@ -0,0 +1,44 @@
+(function($){
+	function build_promises(args){
+		var promises = [];
+		
+		for (var i = 0; i < args.length; i++){
+			var fn = args[i];
+			promises.push(fn.call());
+		}
+		
+		return promises;
+	}
+
+	$.DeferredQueue = function(){
+		var queue = [];
+		var promise = null;
+		var added_callback = null;
+
+		function create(funcs){
+			var promises = build_promises(funcs);
+			return $.when.apply($, promises).then(dequeue);;
+		}
+
+		function dequeue(){
+			if (!queue.length)
+				promise = null;
+			else
+				return create(queue.shift());
+		}
+
+		return {
+			queue:function(){
+				var args = Array.prototype.slice.call(arguments);
+				
+				if (promise){
+					queue.push(args);
+				} else {
+					promise = create(args);
+				}
+
+				return this;
+			}
+		};
+	};	
+})(jQuery);
